11bcf7a1292cb72c3834ababb2ab0ecbbc0f6859
naty <naty.12@gmail.com>
2020-02-06 14:51:37 -0500

    Trabajo de construccion de software1 semana 01

--- src/SEMANA01_TEORICO/BufferReader.java
+++ src/SEMANA01_TEORICO/BufferReader.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package SEMANA02_TEORICO;
+package SEMANA01_TEORICO;
 
 import java.io.BufferedReader;
 import java.io.IOException;

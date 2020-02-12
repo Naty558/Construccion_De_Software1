722b6c44f43b8843e319b8b2610b3203ab4aac0e
naty <naty.12@gmail.com>
2020-02-12 08:56:26 -0500

    Trabajo de construccion de software1 semana 01 y semana02 y trabajo word

---
+++ src/SEMANA02_PRACTICO/Ejericio1_A.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SEMANA02_PRACTICO;
+
+public class Ejericio1_A {
+      public static void main(String[] args) {
+          boolean x;
+          x = 8 > 5 && 6 > 10 || true ;
+          System.out.println("EL valor de x es :"+x);
+      }
+}
